5d7e5b661a58f237f4767f7a8490df9510d1854e
StepashkaJu <yuliyanosova0807@gmail.com>
2023-04-27 15:51:15 +0400

    add guitar objects

---
+++ constants/constants.js
@@ -0,0 +1,66 @@
+const CATALOG = [
+    {
+        id: 'elemen1',
+        name: 'TERRIS TLP-039 BK',
+        img: 'https://i.ibb.co/MyTCPkV/terris.jpg',
+        price: 13960,
+    },
+{
+        id: 'elemen2',
+        name: 'IBANEZ GIO GRG170DX BKN',
+        img: 'https://i.ibb.co/r6jQ3Pc/ibanez.jpg',
+        price: 33270,
+    },
+{
+        id: 'elemen3',
+        name: 'Fabio LP02 BK',
+        img: 'https://i.ibb.co/4p6vhzk/fabio.jpg',
+        price: 12700,
+    },
+    {
+        id: 'elemen4',
+        name: 'FENDER SQUIER Affinity 2021 Stratocaster HH LRL Olympic White',
+        img: '<https://i.ibb.co/wMn5mzY/fender.jpg',
+        price: 44760,
+    },
+{
+        id: 'elemen5',
+        name: 'EPIPHONE Les Paul Special Satin E1 Ebony Vintage',
+        img: 'https://i.ibb.co/1qphymp/EPIPHONe.jpg',
+        price: 34000,
+    },
+{
+        id: 'elemen6',
+        name: 'ROCKDALE Stars HSS BK',
+        img: 'https://i.ibb.co/HFnnJ2k/ROCKDALE-Stars-HSS-BK.jpg',
+        price: 9500,
+    },
+    {
+        id: 'elemen7',
+        name: 'DENN ST100 WH',
+        img: 'https://i.ibb.co/mzV4ZpL/DENN-ST100-WH.jpg',
+        price: 11100,
+    },
+{
+        id: 'elemen8',
+        name: 'JACKSON X Dinky DKAF 7 Black',
+        img: 'https://i.ibb.co/BcZCL54/JACKSON-X-Dinky-DKAF-7-Black.jpg',
+        price: 96000,
+    },
+{
+        id: 'elemen9',
+        name: 'Cort CR200-FBL Classic Rock',
+        img: 'https://i.ibb.co/KFkFg4y/Cort-CR200-FBL-Classic-Rock.jpg',
+        price: 22700,
+    },
+    {
+        id: 'elemen10',
+        name: 'Solar Guitars A2.6TBR SK',
+        img: 'https://i.ibb.co/cQHzfWV/Solar-Guitars-A2-6-TBR-SK.jpg',
+        price: 99900,
+    },
+];
+
+
+
+
