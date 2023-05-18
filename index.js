b258397d7df7c76f4e35eb14a2e5a5398f4ca64a
StepashkaJu <yuliyanosova0807@gmail.com>
2023-05-18 12:47:56 +0400

    add response check

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function render() {
     const productsCart = localStorageUtils.getProducts();
-    
+
     headerPage.render(productsCart.length);
     productsPage.render();
-    
+
 }
 
 let CATALOG = [];
@@ -11,9 +11,17 @@ let CATALOG = [];
 //http://localhost:3001/guitars
 
 fetch('server/catalog.json') //отправляем запрос на сервер 
-.then(res => res.json())  //для получения результата в формате json
-.then(body => {
-    CATALOG = body; //наш каталог присваивается body и рендерится
-    render();
-})
-.catch(error => console.log(error)) //обработка ошибки
+    //для получения результата в формате json
+    .then((response) => {
+        // Проверяем успешность запроса и выкидываем ошибку
+        if (!response.ok) {
+            throw new Error('Error occurred!')
+        }
+        return response.json()
+    })
+//если с запросом все ок
+    .then(body => {
+        CATALOG = body; //наш каталог присваивается body и рендерится
+        render();
+    })
+    .catch(error => console.log(error)) //обработка ошибки
